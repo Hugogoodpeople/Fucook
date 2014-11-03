5ea2765b2417b969c10afff2aa96f192edd80e3b
Hugo Costa <hugocosta@goodpeople.com>
2014-11-03 17:05:07 +0000

    Initial Commit

---
+++ Fucook/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Fucook
+//
+//  Created by Hugo Costa on 03/11/14.
+//  Copyright (c) 2014 Hugo Costa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FucookTests/FucookTests.m
@@ -0,0 +1,40 @@
+//
+//  FucookTests.m
+//  FucookTests
+//
+//  Created by Hugo Costa on 03/11/14.
+//  Copyright (c) 2014 Hugo Costa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface FucookTests : XCTestCase
+
+@end
+
+@implementation FucookTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
