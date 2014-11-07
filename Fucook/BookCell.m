41549ca95a53eda3078508cc3370ceede654b573
Hugo Costa <hugocosta@goodpeople.com>
2014-11-07 17:01:47 +0000

    para o vitror

---
+++ Fucook/BookCell.m
@@ -0,0 +1,23 @@
+//
+//  BookCell.m
+//  Fucook
+//
+//  Created by Hugo Costa on 07/11/14.
+//  Copyright (c) 2014 Hugo Costa. All rights reserved.
+//
+
+#import "BookCell.h"
+
+@implementation BookCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
