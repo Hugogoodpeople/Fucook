4183ea3d0e0d4f38e899783a7b4c6c300e9fcc43
VitorPereiraM <user@imac-de-user.lan>
2014-11-07 17:39:06 +0000

    NewReceita

---
+++ Fucook/NewReceita/NewReceita.h
@@ -0,0 +1,13 @@
+//
+//  NewReceita.h
+//  Fucook
+//
+//  Created by Rundlr on 07/11/14.
+//  Copyright (c) 2014 Hugo Costa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewReceita : UIViewController
+
+@end
