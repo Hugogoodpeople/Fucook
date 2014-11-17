ee1a75500e74dfb7ab5d0c1c50ae95fcf377d9f8
Hugo Costa <hugocosta@goodpeople.com>
2014-11-17 09:24:27 +0000

    Novos ficheiros

---
+++ Fucook/ListaCompras/ListaComprasCell.h
@@ -0,0 +1,13 @@
+//
+//  ListaComprasCell.h
+//  Fucook
+//
+//  Created by Hugo Costa on 17/11/14.
+//  Copyright (c) 2014 Hugo Costa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ListaComprasCell : UITableViewCell
+
+@end
---
+++ Fucook/ListaCompras/ObjectLista.m
@@ -0,0 +1,13 @@
+//
+//  ObjectLista.m
+//  Fucook
+//
+//  Created by Hugo Costa on 17/11/14.
+//  Copyright (c) 2014 Hugo Costa. All rights reserved.
+//
+
+#import "ObjectLista.h"
+
+@implementation ObjectLista
+
+@end
