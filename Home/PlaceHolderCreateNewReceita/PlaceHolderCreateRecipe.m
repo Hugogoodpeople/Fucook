34dad2aef556b8bc5c0af71d20c9efe44b238a6d
Hugo Costa <hugocosta@goodpeople.com>
2014-11-26 12:49:45 +0000

    corrigidos tamanhos placeholder receitas
    em diferentes dispositivos o tamanho do placeholder varia

--- Home/PlaceHolderCreateNewReceita/PlaceHolderCreateRecipe.m
+++ Home/PlaceHolderCreateNewReceita/PlaceHolderCreateRecipe.m
@@ -18,6 +18,9 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     
+    
+    // se tem mascara nao adianta meter sombra porque nao aparece
+    /*
     UIColor *color = [UIColor colorWithRed:53.0/255.0 green:54.0/255.0 blue:58.0/255.0 alpha:1];
     
     self.ViewMovel.layer.shadowColor = [color CGColor];
@@ -25,11 +28,45 @@
     self.ViewMovel.layer.shadowOpacity = .1;
     self.ViewMovel.layer.shadowOffset = CGSizeMake(5, 5);
     self.ViewMovel.layer.masksToBounds = NO;
+    */
+    
+  
+    float altura = [self calcularAltura];
+    
+    UIImage *_maskingImage = [UIImage imageNamed:@"mascara_transparente.png"];
+    CALayer *_maskingLayer = [CALayer layer];
+    _maskingLayer.frame = CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width -30 , altura);
+    [_maskingLayer setContents:(id)[_maskingImage CGImage]];
+    [self.ViewMovel.layer setMask:_maskingLayer];
+
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(float)calcularAltura
+{
+    int alturaEcra = [UIScreen mainScreen].bounds.size.height;
+    int devolver;
+    
+    if (alturaEcra == 480)
+    {
+        devolver = 275;
+    }else if (alturaEcra == 568)
+    {
+        devolver = 340;
+    }else if (alturaEcra == 667)
+    {
+        devolver = 430;
+    }
+    else if (alturaEcra == 736)
+    {
+        devolver = 485;
+    }
+    
+    return devolver;
 }
 
 /*
